b9575191b9dfe7fbdd02dfdaebdaae3eb0dfa986
Luis Briceno <luijo@caro-1315.localhost>
2018-11-28 08:06:01 -0500

    Change Dockerfile phpfpm #30

--- ubuntu/back_phpfpm/build/init_service.sh
+++ ubuntu/back_phpfpm/build/init_service.sh
@@ -4,7 +4,7 @@
 /etc/php7/sbin/php-fpm --fpm-config /etc/php7/etc/php-fpm.conf
 
 # Iniciar apache2
-./apache2ctl -D
+apache2ctl -D
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start apache2ctl: $status"
